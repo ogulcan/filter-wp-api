759e161f22ffcdfb734edfeb268e2b61af0ea149
Ogulcan <junkchorn@gmail.com>
2017-09-22 19:14:38 +0300

    Added date field for compact

--- filter-wp-api/public/class-filter-wp-api-public.php
+++ filter-wp-api/public/class-filter-wp-api-public.php
@@ -77,6 +77,7 @@ class Filter_WP_Api_Public {
 		            'id'        => $data->data['id'],
 		            'title'     => $data->data['title']['rendered'],
 		            'link'      => $data->data['link'],
+		            'date'		=> $data->data['modified'],
 		            'image'     => $featured_image_url[0]
 		        ];
 			}
